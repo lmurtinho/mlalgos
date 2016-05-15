d2dd223238abcfd910688065121aee1fd752640e
Lucas Murtinho <lucas.murtinho@gmail.com>
2016-05-14 23:06:29 -0300

    linear regression with tests comparing to sklearn result in boston dataset with and without intercept

--- sup_learn/lin_reg/lin_reg.py
+++ sup_learn/lin_reg/lin_reg.py
@@ -1,1 +1,29 @@
-#TODO
+# -*- coding: utf-8 -*-
+"""
+Linear regression function
+"""
+
+import numpy as np
+
+def linear_regression(X, y, intercept=True):
+    """
+    Function to perform linear regression
+    on a set of features X to explain a 
+    target y.
+    Returns the coefficients for a linear
+    regression of y onto X.
+    """
+    # add intercept column to X
+    if intercept:
+        X = np.c_[np.ones(X.shape[0]), X]
+
+    # X*theta = y and we want to isolate theta, so:
+    # get X transposed (so that Xt*X*theta = Xt*y)
+    Xt = X.transpose()
+    
+    # get inverse of Xt*X  to do
+    # ((Xt*X)**-1)*(Xt*X)*theta = ((Xt*X)**-1)*Xt*y or
+    # I*theta = ((Xt*X)**-1)*Xt*y
+    X_inv = np.linalg.inv(Xt.dot(X))    
+        
+    return X_inv.dot(Xt).dot(y)
---
+++ sup_learn/lin_reg/test_lin_reg.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 14 21:23:14 2016
+
+Tests for the linear regression function.
+
+"""
+
+from nose import run
+from nose.tools import assert_true
+from sklearn.datasets import load_boston
+from sklearn.linear_model import LinearRegression
+
+import numpy as np
+
+from lin_reg import linear_regression
+
+boston = load_boston()
+X = boston.data
+y = boston.target
+
+def test_boston():
+    sk_lr = LinearRegression()
+    sk_lr.fit(X, y)
+    sk_lr_result = np.insert(sk_lr.coef_, 0, sk_lr.intercept_)
+    my_lr = linear_regression(X, y)
+    assert_true(np.allclose(sk_lr_result, np.array(my_lr)))
+
+def test_boston_no_interc():
+    sk_lr = LinearRegression(fit_intercept=False)
+    sk_lr.fit(X, y)
+    my_lr = linear_regression(X, y, False)
+    assert_true(np.allclose(sk_lr.coef_, np.array(my_lr)))
+    
+
+run()
